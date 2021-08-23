73442e9000c76458a1253e9127312e85e7ac2dc5
jatinsaini137 <jatinrajbagri2001@gmail.com>
2021-08-23 23:43:41 +0530

    palindrome using recurion

---
+++ Frequent/Recursion/isPalindrome.py
@@ -0,0 +1,17 @@
+# isPalindrome Solution
+
+
+def isPalindrome(strng):
+    if len(strng) == 0:
+        return True
+    if strng[0] != strng[len(strng)-1]:
+        return False
+    return isPalindrome(strng[1:-1])
+
+#Example
+
+print(isPalindrome('awesome')) # false
+print(isPalindrome('foobar')) # false
+print(isPalindrome('tacocat')) # true
+print(isPalindrome('amanaplanacanalpanama')) # true
+print(isPalindrome('amanaplanacanalpandemonium')) # false
